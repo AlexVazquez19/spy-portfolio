86d7cb65c9f67b5d2f68e851f0e4b6d0a5b79476
Alex Vazquez <86645097+AlexVazquez19@users.noreply.github.com>
2022-11-12 23:25:11 -0800

    Add files via upload

---
+++ pie_chart.py
@@ -0,0 +1,56 @@
+from tkinter import *
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
+NavigationToolbar2Tk)
+
+# DATA
+sector_names = ['Information Technology','Health Care','Consumer Discretionary',
+'Communication Services','Financials','Industrials','Consumer Staples',
+'Utilities','Materials','Real Estate','Energy']
+
+sector_weights = []
+def updateChartNumbers():
+    global sector_weights
+    with open('sector_weights.txt','r') as f:
+        data = f.read().splitlines()
+        sector_weights = []
+        for elem in data:
+            sector_weights.append(float(elem))
+
+updateChartNumbers()
+
+colors = ['#001d6c','#002d9c','#0043ce','#0f62fe','#4589ff','#78a9ff','#a56eff',
+    '#8a3ffc','#6929c4','#491d8b','#31135e']
+
+#SETUP PIE CHART
+fig, ax = plt.subplots(figsize=(7,4),dpi=40)
+
+patches, texts, pcts = ax.pie(
+    sector_weights, labels=sector_names, autopct='%.1f%%',
+    wedgeprops={'linewidth': 0, 'edgecolor': 'white'},
+    colors=colors)
+
+plt.setp(pcts, color='white', fontweight='bold')
+
+# FRAMES
+frm_chart = Frame()
+frm_chart.pack(side='right')
+
+# CONFIGURE rows and columns in frames
+canvas = FigureCanvasTkAgg(fig, master=frm_chart)
+canvas.draw()
+
+# COMMANDS
+
+def redraw():
+    return
+    #TODO
+
+# WIDGETS
+
+# PACKING / GRIDDING
+canvas.get_tk_widget().pack(padx=30,pady=30)
+
+# BINDS
+
