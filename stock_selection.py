bb4b5023697355d4840216e3255db6b657892e6c
AlexVazquez19 <86645097+AlexVazquez19@users.noreply.github.com>
2023-01-25 23:03:43 -0800

    Added black border around 'my portfolio' frame

--- stock_selection.py
+++ stock_selection.py
@@ -7,9 +7,9 @@ portfolio_stocks = ['MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD',
 
 # FRAMES
 frm_container = Frame()
-frm_container.grid(row=3,column=0,columnspan=2,sticky="w",pady=(25,0))
+frm_container.grid(row=3,column=0,columnspan=2,sticky="w",pady=(25,0),padx=10)
 
-frm_mystocks = Frame(master=frm_container)
+frm_mystocks = Frame(master=frm_container,highlightbackground="black",highlightthickness=1.5)
 frm_mystocks.grid(row=0,column=0)
 
 # CONFIGURE rows and columns in frames
@@ -22,8 +22,8 @@ def updatePortfolio():
         lbl_stock.grid(row=r,column=0,sticky="w",padx=10)
         r+=1
 
-# SCROLL WINDOWS
-canvas_portfolio = Canvas(frm_mystocks)
+# SCROLL WINDOW
+canvas_portfolio = Canvas(frm_mystocks,width=80, height=200)
 vscroll = Scrollbar(frm_mystocks,orient='vertical',command=canvas_portfolio.yview)
 scrollable_frame = Frame(canvas_portfolio)
 
@@ -41,12 +41,12 @@ canvas_portfolio.configure(yscrollcommand=vscroll.set)
 # WIDGETS
 bold_font = font.Font(weight='bold')
 
-lbl_myportfolio = Label(master=scrollable_frame,text='My Portfolio',font=bold_font)
+lbl_myportfolio = Label(master=frm_mystocks,text='My Portfolio',font=bold_font)
 
 # PACKING / GRIDDING
+lbl_myportfolio.pack(side="top",padx=10)
 canvas_portfolio.pack(side="left", fill="both", expand=True)
 vscroll.pack(side="right", fill="y")
-lbl_myportfolio.grid(row=0,column=0,sticky="w",padx=10)
 
 # BINDS
 updatePortfolio()
