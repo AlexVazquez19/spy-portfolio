820cdfc5fb0a5190180834b739f4915f62a94153
AlexVazquez19 <86645097+AlexVazquez19@users.noreply.github.com>
2023-01-25 22:34:38 -0800

    Added scrollbar to 'my portfolio' section

--- stock_selection.py
+++ stock_selection.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from tkinter import font
 
+
 # DATA
-portfolio_stocks = ['MSFT','AAPL','AMD']
+portfolio_stocks = ['MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD','MSFT','AAPL','AMD']
 
 # FRAMES
 frm_container = Frame()
@@ -17,16 +18,34 @@ frm_mystocks.grid(row=0,column=0)
 def updatePortfolio():
     r = 1
     for stock in portfolio_stocks:
-        lbl_stock = Label(master=frm_mystocks,text=stock)
+        lbl_stock = Label(master=scrollable_frame,text=stock)
         lbl_stock.grid(row=r,column=0,sticky="w",padx=10)
         r+=1
+
+# SCROLL WINDOWS
+canvas_portfolio = Canvas(frm_mystocks)
+vscroll = Scrollbar(frm_mystocks,orient='vertical',command=canvas_portfolio.yview)
+scrollable_frame = Frame(canvas_portfolio)
+
+scrollable_frame.bind(
+    "<Configure>",
+    lambda e: canvas_portfolio.configure(
+        scrollregion=canvas_portfolio.bbox("all")
+    )
+)
+
+canvas_portfolio.create_window((0, 0), window=scrollable_frame, anchor="nw")
+canvas_portfolio.configure(yscrollcommand=vscroll.set)
+
 
 # WIDGETS
 bold_font = font.Font(weight='bold')
 
-lbl_myportfolio = Label(master=frm_mystocks,text='My Portfolio',font=bold_font)
+lbl_myportfolio = Label(master=scrollable_frame,text='My Portfolio',font=bold_font)
 
 # PACKING / GRIDDING
+canvas_portfolio.pack(side="left", fill="both", expand=True)
+vscroll.pack(side="right", fill="y")
 lbl_myportfolio.grid(row=0,column=0,sticky="w",padx=10)
 
 # BINDS
